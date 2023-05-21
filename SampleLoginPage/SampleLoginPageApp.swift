67cf1d5ff237ad04f84a888e4adac2661c13c830
mahmutkara01 <bilgimahmutkara@gmail.com>
2023-05-21 18:35:44 +0300

    Initial Commit

---
+++ SampleLoginPage/SampleLoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleLoginPageApp.swift
+//  SampleLoginPage
+//
+//  Created by mahmut kara on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SampleLoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
